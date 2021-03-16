72262a167204bfa9b661d79ed28202e20d6d770a
ayotayu <taiki4170@icloud.com>
2021-03-17 08:37:25 +0900

    add static page home help

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-    root 'application#hello'
-  end
+  root 'application#hello'
+  
 end
